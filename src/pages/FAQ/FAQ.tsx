65c8b2786d27fdd6aadb0a6f4a2c0bb746ef21bb
Andres Izbri <aizbri@stepservizi.net>
2024-05-02 17:24:10 +0200

    Feat: Home page and styiling of other pages

---
+++ src/pages/FAQ/FAQ.tsx
@@ -0,0 +1,11 @@
+import FaqWithHeader from '@/components/FAQComponents/FAQPageHeader/FaqWithHeader';
+
+const FAQPage = () => {
+  return (
+    <>
+      <FaqWithHeader />
+    </>
+  );
+};
+
+export default FAQPage;
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import { FeaturesCards } from '@/components/Features/FeaturesCards';
+import { HeroText } from '@/components/Hero/HeroText';
+
+const HomePage = () => {
+  return (
+    <div>
+      <HeroText />
+      <FeaturesCards />
+    </div>
+  );
+};
+
+export default HomePage;
